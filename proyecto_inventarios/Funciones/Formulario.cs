f374b19b9ca1d968cb2b43dbfe6b6959fe68578e
AngelMelecio <joseangelsm50@gmail.com>
2023-11-06 14:34:55 -0600

    Agregar archivos de proyecto.

---
+++ proyecto_inventarios/Funciones/Formulario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto_inventarios.Funciones
+{
+    internal class Formulario
+    {
+        
+        public double NumPzsOrdenar(int D, int K, int n)
+        {
+            return Math.Sqrt((2 * D * K) / n); 
+        }
+
+    }
+
+}
